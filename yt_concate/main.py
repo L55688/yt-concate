c05dbf890c20dcedfc9575c9de96b5a82b2d2e78
Roy Liu <roy004847@gmail.com>
2025-08-19 16:15:39 +0800

    change main.py

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,58 +1,43 @@
-import urllib.request
-import json
+from pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.initialize_yt import InitializeYT
+from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.read_caption import ReadCaption
+from yt_concate.pipeline.steps.search import Search
+from yt_concate.pipeline.steps.download_videos import DownloadVideos
+from yt_concate.pipeline.steps.edit_video import EditVideo
+from yt_concate.pipeline.steps.postflight import Postflight
+from yt_concate.pipeline.steps.step import StepException
+from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.utils import Utils
 
-from yt_concate.pipeline.steps.step import Step
-from yt_concate.settings import API_KEY
+CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 
-class GetVideoList(Step):
-    def process(self, data, inputs, utils):
-        print('in get video list')  # 加入了此行，方便同學Debug
-        print(API_KEY)  # 加入了此行，方便同學Debug
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID,
+        'search_word': 'incredible',
+        'limit': 20,
+    }
 
-        channel_id = inputs['channel_id']
 
-        if utils.video_list_file_exists(channel_id):
-            print('Found existing video list file for channel id', channel_id)
-            return self.read_file(utils.get_video_list_filepath(channel_id))
+    steps = [
+        Preflight(),
+        GetVideoList(),
+        InitializeYT(),
+        DownloadCaptions(),
+        ReadCaption(),
+        Search(),
+        DownloadVideos(),
+        EditVideo(),
+        Postflight(),
+        ]
 
-        base_video_url = 'https://www.youtube.com/watch?v='
-        base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+    utils = Utils()
+    p = Pipeline(steps)
+    p.run(inputs, utils)
 
-        first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
 
-                                                                                              channel_id)
-
-        print(first_url)  # 加入了此行，方便同學Debug，遇403 Error者，請把印出的網址貼到瀏覽器中檢視，會寫錯誤原因
-
-        video_links = []
-        url = first_url
-        while True:
-            inp = urllib.request.urlopen(url)
-            resp = json.load(inp)
-
-            for i in resp['items']:
-                if i['id']['kind'] == "youtube#video":
-                    video_links.append(base_video_url + i['id']['videoId'])
-
-            try:
-                next_page_token = resp['nextPageToken']
-                url = first_url + '&pageToken={}'.format(next_page_token)
-            except KeyError:
-                break
-
-        print(video_links)
-        self.write_to_file(video_links, utils.get_video_list_filepath(channel_id))
-        return video_links
-
-    def write_to_file(self, video_links, filepath):
-        with open(filepath, 'w') as f:
-            for url in video_links:
-                f.write(url + '\n')
-
-    def read_file(self, filepath):
-        video_links = []
-        with open(filepath, 'r') as f:
-            for url in f:
-                video_links.append(url.strip())
-        return video_links
+if __name__ == '__main__':
+    main()
